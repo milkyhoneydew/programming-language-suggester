95ea8779cbefdf7611f145b9f67ac22f822c4e7c
Tanya Prado <tanyaprado@gmail.com>
2022-11-03 20:03:56 -0700

    set hidden attribute and gather input text

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,27 @@ window.addEventListener("load", function() {
     const form = document.querySelector("form");
 
     form.addEventListener("submit", function(event) {
-        let question1 = document.getElementById("question1");
-        let question2 = document.getElementById("question2");
-        let question3 = document.getElementById("question3");
-        let question4 = document.getElementById("question4");
-        let question5 = document.getElementById("question5");
+        let javascript = document.getElementById("javascript");
+        let python = document.getElementById("python");
+        let sql = document.getElementById("sql");
 
-        question1.setAttribute("class", "hidden");
-        question2.setAttribute("class", "hidden");
-        question3.setAttribute("class", "hidden");
-        question4.setAttribute("class", "hidden");
-        question5.setAttribute("class", "hidden");
+        javascript.setAttribute("class", "hidden");
+        python.setAttribute("class", "hidden");
+        sql.setAttribute("class", "hidden");
+        
+        const question1 = document.querySelector("input#question1").value;
+        const question2 = document.querySelector("input#question2").value;
+        const question3 = document.querySelector("input#question3").value;
+        const question4 = document.querySelector("input#question4").value;
+        const question5 = document.querySelector("input#question5").value;
+
+        if (question === front-end) {
+            javascript.removeAttribute("class");
+        } else if ()
+
     });
 
-    
+
 
 
 });
